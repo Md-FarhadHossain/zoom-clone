b8c3e95619a6f880428af70df22b9f7e761dae21
Md-FarhadHossain <freelancerfarhadhossain@gmail.com>
2024-03-31 01:20:18 +0600

    navbar logo added

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,20 @@
+import Image from "next/image";
+import Link from "next/link";
+
 const Navbar = () => {
   return (
     <nav className="flex justify-between z-50 w-full bg-dark-1 px-6 py-4 lg:px-10">
-      
+      <Link href="/" className="flex items-center gap-1">
+        <Image
+          src="/icons/logo.svg"
+          alt="Zoom Clone Logo"
+          width={32}
+          height={32}
+          className="max-sm:size-10"
+        />
+        <p className="text-[26px] font-extrabold text-white max-sm:hidden">Zoom</p>
+      </Link>
     </nav>
-  )
-}
-export default Navbar
+  );
+};
+export default Navbar;
